0516d1ae4c7c0c62c745c02481aede0d6c44ca7f
shahzaibk23 <shahzaibceo@gmail.com>
2024-10-03 04:09:32 +0500

    Abstraction for all Cores -- to be generic more

---
+++ src/main/scala/Cores.scala
@@ -0,0 +1,16 @@
+import chisel3._
+import chisel3.experimental.Analog
+import chisel3.stage.ChiselStage
+
+import caravan.bus.common._
+
+import nucleusrv.components.{Core,CoreIO, Configs}
+
+// abstract class AbstractCoreIO extends CoreIO
+
+abstract trait AbstractCore
+    // val io: AbstractCoreIO
+
+class NRV(override implicit val config:Configs) extends Core with AbstractCore{
+    // val io:CoreIO
+}
